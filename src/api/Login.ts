ac9abf7369af31222bd18907cc93f13f4588ae5e
Thanh Le <thle@deloitte.com.au>
2021-08-10 16:57:38 +1000

    lint and format

--- src/api/Login.ts
+++ src/api/Login.ts
@@ -2,20 +2,20 @@ import * as fs from 'fs';
 import { parseStringPromise } from 'xml2js';
 
 class SoapLogin {
-  async login(role: { username: string; password: string; token?: string; }) {
-    let envelope = await this.constructEnvelope(role)
+  async login(role: { username: string; password: string; token?: string }) {
+    const envelope = await this.constructEnvelope(role);
   }
 
-  async constructEnvelope(role: { username: string; password: string; token?: string}) {
-    if (role.username == '' || role.password == '') {
-      throw new Error('Missing login details!')
+  async constructEnvelope(role: { username: string; password: string; token?: string }) {
+    if (role.username === '' || role.password === '') {
+      throw new Error('Missing login details!');
     }
-    let abstractEnvelope = await this.loadEnvelop();
-    abstractEnvelope['env:Envelope']['env:Body'][0]['n1:login'][0]['n1:username'] = role.username
-    if (role.token != undefined) {
-      abstractEnvelope['env:Envelope']['env:Body'][0]['n1:login'][0]['n1:password'] = `${role.password}${role.token}`
+    const abstractEnvelope = await this.loadEnvelop();
+    abstractEnvelope['env:Envelope']['env:Body'][0]['n1:login'][0]['n1:username'] = role.username;
+    if (role.token !== undefined) {
+      abstractEnvelope['env:Envelope']['env:Body'][0]['n1:login'][0]['n1:password'] = `${role.password}${role.token}`;
     } else {
-      abstractEnvelope['env:Envelope']['env:Body'][0]['n1:login'][0]['n1:password'] = role.password
+      abstractEnvelope['env:Envelope']['env:Body'][0]['n1:login'][0]['n1:password'] = role.password;
     }
     return abstractEnvelope;
   }
@@ -24,8 +24,6 @@ class SoapLogin {
     let envelop = fs.readFileSync(process.cwd() + '/src/api/resources/login-env.xml');
     return parseStringPromise(envelop);
   }
-
-
 }
 
 export default new SoapLogin();
