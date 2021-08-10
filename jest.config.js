4bb2c47bb56df88c7bd602ac25497d7c25edcdc4
Thanh Le <thle@deloitte.com.au>
2021-08-11 06:30:22 +1000

    test unit coverage

--- jest.config.js
+++ jest.config.js
@@ -19,10 +19,10 @@ module.exports = {
   "coverageReporters": ["json", "lcov", "text", "clover", "cobertura"],
   coverageThreshold: {
     global: {
-      statements: 50,
+      statements: 70,
       branches: 80,
-      functions: 0,
-      lines: 0,
+      functions: 80,
+      lines: 70,
     },
   }
   // setupFiles: ['./jest.setup-file.ts'],
