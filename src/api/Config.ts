78a69a53219a4b82763cdfc8a70a79bee663eeb2
Thanh Le <thle@deloitte.com.au>
2021-08-27 15:48:46 +1000

    move dependencies and support JSON as input

--- src/api/Config.ts
+++ src/api/Config.ts
@@ -2,9 +2,13 @@ import * as fs from 'fs';
 
 export default class Config {
   private config: JSON;
-  constructor(configFile: string) {
-    const content = fs.readFileSync(configFile, 'utf-8');
-    this.config = JSON.parse(content);
+  constructor(configFile: string | JSON) {
+    if (typeof configFile === 'string') {
+      const content = fs.readFileSync(configFile, 'utf-8');
+      this.config = JSON.parse(content);
+    } else {
+      this.config = configFile;
+    }
   }
 
   getConfig() {
