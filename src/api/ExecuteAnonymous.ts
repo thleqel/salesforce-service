3723e1560d1ac828ea098e90739c196321033789
Thanh Le <thle@deloitte.com.au>
2021-10-01 11:50:44 +1000

    execute anonymous scripts

---
+++ src/api/ExecuteAnonymous.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { SfConfig } from './interfaces/Config';
+
+class ExecuteAnonymous {
+  async executeAnonymous(script: string, config: SfConfig) {
+    const envelope = await this.constructEnvelope(script);
+    const soapPath = config.paths.soap;
+    const soapServicePath = soapPath.replace('services/Soap/u/', 'services/Soap/s/')
+    const conf: AxiosRequestConfig = {
+      baseURL: config.urls.baseUrl,
+      url: soapServicePath,
+      method: 'post',
+      headers: {
+        'Content-Type': 'text/xml',
+        SOAPAction: 'blank',
+        'Accept-Encoding': 'gzip, deflate, br',
+      },
+      data: envelope,
+    };
+    return axios.request(conf);
+  }
+
+  async constructEnvelope(script: string) {
+    let abstractEnvelope: string = this.loadEnvelop();
+    abstractEnvelope = abstractEnvelope.replace(
+      '<apex:sessionId></apex:sessionId>',
+      `<apex:sessionId>${process.env.SESSION_ID}</apex:sessionId>`,
+    );
+
+    abstractEnvelope = abstractEnvelope.replace(
+      '<apex:String></apex:String>',
+      `<apex:String>${script}</apex:String>`,
+    );
+    return abstractEnvelope;
+  }
+
+  loadEnvelop() {
+    return `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:apex="http://soap.sforce.com/2006/08/apex">
+    <soapenv:Header>
+        <apex:DebuggingHeader>
+            <apex:categories>
+                <apex:category>Apex_code</apex:category>
+                <apex:level>ERROR</apex:level>
+            </apex:categories>
+            <apex:debugLevel>NONE</apex:debugLevel>
+        </apex:DebuggingHeader>
+        <apex:SessionHeader>
+            <apex:sessionId></apex:sessionId>
+        </apex:SessionHeader>
+    </soapenv:Header>
+    <soapenv:Body>
+        <apex:executeAnonymous>
+            <apex:String></apex:String>
+        </apex:executeAnonymous>
+    </soapenv:Body>
+    </soapenv:Envelope>`;
+  }
+}
+
+export default new ExecuteAnonymous();
